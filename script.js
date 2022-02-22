559d610e3bb57a705bc5bbca8e7720f8e7fa4435
Vidotti <vidottizzz@gmail.com>
2022-02-22 13:46:40 -0300

    Funcionalidade Javascript quase completa

--- script.js
+++ script.js
@@ -5,65 +5,42 @@ let weekly_previous = [];
 let monthly_current = [];
 let monthly_previous = [];
 
-let hours = document.querySelectorAll('.hours');
-let previousHours = document.querySelectorAll('.previous-hours');
-let buttons = document.querySelectorAll('.button');
+let hours = document.querySelectorAll(".hours");
+let previousHours = document.querySelectorAll(".previous-hours");
+let spans = document.querySelectorAll(".spans");
+let btns = document.querySelectorAll(".button");
 
 
+for (var i = 0; i < spans.length; i++) {
+    spans[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
 
-fetch('data.json')
-.then(res => res.json())
-.then(data => {
-    // se colocar o 0 é referente a sessão WORK
-    // se colocar o 1 é referente a sessão PLAY e assim consecutivamente
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    for (let i = 0; i <= 6; i++) {
+        
+      daily_current.push(data[i].timeframes.daily.current);
+      daily_previous.push(data[i].timeframes.daily.previous);
+      weekly_current.push(data[i].timeframes.weekly.current);
+      weekly_previous.push(data[i].timeframes.weekly.previous);
+      monthly_current.push(data[i].timeframes.monthly.current);
+      monthly_previous.push(data[i].timeframes.monthly.previous);
 
-    for (let i = 0; i <= 6 ; i++) {
-        daily_current.push(data[i].timeframes.daily.current);
-        daily_previous.push(data[i].timeframes.daily.previous);
-        weekly_current.push(data[i].timeframes.weekly.current);
-        weekly_previous.push(data[i].timeframes.weekly.previous);
-        monthly_current.push(data[i].timeframes.monthly.current);
-        monthly_previous.push(data[i].timeframes.monthly.previous);
 
-        // quando tiver no weekly mostra ese código
+      if (spans[0].classList.contains("active")) {
+        hours[i].innerHTML = `${daily_current[i]}hrs`;
+        previousHours[i].innerHTML = `Yesterday - ${daily_previous[i]}hrs`;
+      } else if (spans[1].classList.contains("active")) {
         hours[i].innerHTML = `${weekly_current[i]}hrs`;
         previousHours[i].innerHTML = `Last Week - ${weekly_previous[i]}hrs`;
-
-
-    
-        // IF O BOTÃO DAILY ESTÁ ATIVO COLOCA A OUTRA COLUNA DE CÓDIGO
-
-        // hours[i].innerHTML = `${daily_current[i]}hrs`
-        // previousHours[i].innerHTML = `Yesterday - ${daily_previous[i]}hrs`;
-
-        // IF O BOTÃO MONTHLY ESTÁ ATIVO COLOCA A OUTRA COLUNA DE CÓDIGO
-
-        // hours[i].innerHTML = `${daily_current[i]}hrs`
-        // previousHours[i].innerHTML = `Yesterday - ${daily_previous[i]}hrs`
-                          
-    }   
-});
-
-console.log(daily_current);
-console.log(daily_previous);
-console.log(weekly_current);
-console.log(weekly_previous);
-console.log(monthly_current);
-console.log(monthly_previous);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      } if (spans[2].classList.contains("active")) {
+        hours[i].innerHTML = `${monthly_current[i]}hrs`;
+        previousHours[i].innerHTML = `Last Month - ${monthly_previous[i]}hrs`;
+      }
+    }
+  });
+}
