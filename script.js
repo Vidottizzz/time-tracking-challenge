c11c125cdc4d04952b53797bcfb70fa4df3f4728
Vidotti <vidottizzz@gmail.com>
2022-02-23 16:18:09 -0300

    Funcionalidade Javascript completa

--- script.js
+++ script.js
@@ -1,51 +1,82 @@
-let daily_current = [];
-let daily_previous = [];
-let weekly_current = [];
-let weekly_previous = [];
-let monthly_current = [];
-let monthly_previous = [];
+const daily_current = [];
+const daily_previous = [];
+const weekly_current = [];
+const weekly_previous = [];
+const monthly_current = [];
+const monthly_previous = [];
 
-let hours = document.querySelectorAll(".hours");
-let previousHours = document.querySelectorAll(".previous-hours");
-var activedElement;
+const $spans = document.querySelectorAll(".spans");
+const $btns = document.querySelectorAll(".button");
 
-let spans = document.querySelectorAll(".spans");
-let btns = document.querySelectorAll(".button");
-
-for (var i = 0; i < spans.length; i++) {
-  spans[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
-  });
-}
-
-
+// for (var i = 0; i < $spans.length; i++) {
+//   $spans[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace("active", "");
+//     this.className += " active";
+//   });
+// }
 
 fetch("data.json")
-  .then(res => res.json())
-  .then(data => { 
-       data.forEach(element => {
-        daily_current.push(element.timeframes.daily.current);
-        daily_previous.push(element.timeframes.daily.previous);
-        weekly_current.push(element.timeframes.weekly.current);
-        weekly_previous.push(element.timeframes.weekly.previous);
-        monthly_current.push(element.timeframes.monthly.current);
-        monthly_previous.push(element.timeframes.monthly.previous);   
-       })   
+  .then((response) => response.json())
+  .then((json_data) => {
+    json_data.forEach((element) => {
+      daily_current.push(element.timeframes.daily.current);
+      daily_previous.push(element.timeframes.daily.previous);
+      weekly_current.push(element.timeframes.weekly.current);
+      weekly_previous.push(element.timeframes.weekly.previous);
+      monthly_current.push(element.timeframes.monthly.current);
+      monthly_previous.push(element.timeframes.monthly.previous);
+    });
   })
-  .then(result => {
-    for (let i = 0; i <= 6; i++) {  
-      if (spans[0].classList.contains("active")) {
-        hours[i].innerHTML = `${daily_current[i]}hrs`;
-        previousHours[i].innerHTML = `Yesterday - ${daily_previous[i]}hrs`;
-      } else if (spans[1].classList.contains("active")) {
-        hours[i].innerHTML = `${weekly_current[i]}hrs`;
-        previousHours[i].innerHTML = `Last Week - ${weekly_previous[i]}hrs`;
-      } else if (spans[2].classList.contains("active")) {
-        hours[i].innerHTML = `${monthly_current[i]}hrs`;
-        previousHours[i].innerHTML = `Last Month - ${monthly_previous[i]}hrs`;
-      }
+  .then((result) => {
+    const currentHours = document.querySelectorAll(".hours");
+    const previousHours = document.querySelectorAll(".previous-hours");
+    for (let i = 0; i < 6; i++) {
+      currentHours[i].innerHTML = daily_current[i] + "hrs";
+      previousHours[i].innerHTML =
+        "Yesterday " + " - " + daily_previous[i] + "hrs";
     }
   });
 
+function changeDuration() {
+  let targets = document.getElementsByName("duration");
+  targets.forEach((target) => {
+    target.addEventListener("click", function () {
+      if (target.checked) {
+        let duration = target.value;
+        display(duration);
+      }
+    });
+  });
+}
+
+function display(duration) {
+  const hours = document.getElementsByClassName("hours");
+  const previous_hours = document.getElementsByClassName("previous-hours");
+  let current;
+  let previous;
+  let range;
+  switch (duration) {
+    case "daily":
+       current = daily_current;
+       previous = daily_previous;
+       range = "Yesterday";
+      break;
+    case "weekly":
+      current = weekly_current;
+       range = "Last Week";
+       previous = weekly_previous;
+      break;
+    case "monthly":
+      current = monthly_current;
+      range = "Last Month";
+       previous = monthly_previous;
+      break;
+  }
+  for (let i = 0; i < 6; i++){
+    hours[i].innerHTML = current[i] + "hrs";
+    previous_hours[i].innerHTML = range + " - " + previous[i] + "hrs";
+  }
+}
+
+changeDuration();
