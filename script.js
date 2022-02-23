9f5d8cd1bb7e9295210a27080ad08c2fbab28804
Vidotti <vidottizzz@gmail.com>
2022-02-23 10:38:57 -0300

    trying to solve

--- script.js
+++ script.js
@@ -7,40 +7,42 @@ let monthly_previous = [];
 
 let hours = document.querySelectorAll(".hours");
 let previousHours = document.querySelectorAll(".previous-hours");
+
 let spans = document.querySelectorAll(".spans");
 let btns = document.querySelectorAll(".button");
 
-
 for (var i = 0; i < spans.length; i++) {
-    spans[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
+  spans[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
 
 fetch("data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    for (let i = 0; i <= 6; i++) {
-        
-      daily_current.push(data[i].timeframes.daily.current);
-      daily_previous.push(data[i].timeframes.daily.previous);
-      weekly_current.push(data[i].timeframes.weekly.current);
-      weekly_previous.push(data[i].timeframes.weekly.previous);
-      monthly_current.push(data[i].timeframes.monthly.current);
-      monthly_previous.push(data[i].timeframes.monthly.previous);
-
-
+  .then(res => res.json())
+  .then(data => { 
+       data.forEach(element => {
+        daily_current.push(element.timeframes.daily.current);
+        daily_previous.push(element.timeframes.daily.previous);
+        weekly_current.push(element.timeframes.weekly.current);
+        weekly_previous.push(element.timeframes.weekly.previous);
+        monthly_current.push(element.timeframes.monthly.current);
+        monthly_previous.push(element.timeframes.monthly.previous);   
+       })   
+  })
+  .then(result => {
+    for (let i = 0; i <= 6; i++) {  
       if (spans[0].classList.contains("active")) {
         hours[i].innerHTML = `${daily_current[i]}hrs`;
         previousHours[i].innerHTML = `Yesterday - ${daily_previous[i]}hrs`;
       } else if (spans[1].classList.contains("active")) {
         hours[i].innerHTML = `${weekly_current[i]}hrs`;
         previousHours[i].innerHTML = `Last Week - ${weekly_previous[i]}hrs`;
-      } if (spans[2].classList.contains("active")) {
+      } else if (spans[2].classList.contains("active")) {
         hours[i].innerHTML = `${monthly_current[i]}hrs`;
         previousHours[i].innerHTML = `Last Month - ${monthly_previous[i]}hrs`;
       }
     }
   });
-}
+
